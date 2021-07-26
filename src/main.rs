a86fc5e66e483e16223d6bc36dee0c049bafc694
Husseljo <husseljoo@gmail.com>
2021-07-27 01:00:29 +0200

    parsing more modular

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,33 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> =env::args().collect();
-    let (query, filename) = parse_config(&args);
-    display_content(query,filename);
+    let config = parse_config(&args);
+    display_content(config);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Self{
+        let query=args[1].clone();
+        let filename=args[2].clone();
+        Self { query, filename }
+    }
+}
+
+fn parse_config(args: &[String]) -> Config {
+    Config::new(args)
 }
 
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query=&args[1];
-    let filename=&args[2];
-    (query,filename)
-
-}
-
-
-fn display_content(query: &str, filename: &str) {
-    println!("Searching for {}",query);
-    println!("In file {}",filename);
+fn display_content(config: Config) {
+    println!("Searching for {}",config.query);
+    println!("In file {}",config.filename);
     println!("----------------------\n");
     
-    let contents=fs::read_to_string(filename).expect("Could not read file name!");
+    let contents=fs::read_to_string(config.filename).expect("Could not read file name!");
     println!("Contents of the file are:\n{} ",contents);
 
 }
